08f02949a0063eaaa834493d081e093ff99f5b28
FiyoAfZHQtWcTyzMEaiJ <124754531+FiyoAfZHQtWcTyzMEaiJ@users.noreply.github.com>
2023-05-27 15:25:01 -0500

    updates

--- InstallDuoGPOs.ps1
+++ InstallDuoGPOs.ps1
@@ -4,12 +4,18 @@ $outFile = "C:\temp\DuoGPO.zip"
 Invoke-WebRequest -Uri $url -OutFile $outFile
 Expand-Archive $outFile -DestinationPath "C:\temp\DUO"
 
+Write-Host "Creating Duo GPOs templates..." -ForegroundColor Green
+Start-Sleep 2
 #Copy to PolicyDefinitions
 Copy-Item -Path "C:\temp\DUO\DuoWindowsLogon.adml" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDomain\Policies\PolicyDefinitions\en-US"
 Copy-Item -Path "C:\temp\DUO\DuoWindowsLogon.admx" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDomain\Policies\PolicyDefinitions\"
+#Create DUO GPOs
+New-GPO -Name "Duo-Windows Logon"
+New-GPO -Name "Duo-Installation"
 
 #Copy DUO Installer and create SMD shared folder
-
+Write-Host "Creating Duo SMB Share..." -ForegroundColor Green
+Start-Sleep 2
 New-Item -Path "C:\DUO" -ItemType Directory
 $Parameters = @{
     Name = 'DUO'
@@ -20,27 +26,45 @@ $Parameters = @{
 New-SmbShare @Parameters
 Copy-Item -Path "C:\temp\DUO\DuoWindowsLogon64.msi" -Destination "C:\DUO\"
 
-#Create DUO GPOs
-New-GPO -Name "Duo-Windows Logon"
-New-GPO -Name "Duo-Installation"
-
-Write-Host "Go ahead and begin editing the GPOs created, and link them to the domain." -ForegroundColor Green
-pause
-
 #Download AD Directory Sync
+Write-Host "Starting AD Directory Sync setup..." -ForegroundColor Green
+Start-Sleep 2
 $url = "https://dl.duosecurity.com/duoauthproxy-latest.exe"
 $outFile = "C:\temp\DuoAuthProxy.exe"
 Invoke-WebRequest -Uri $url -OutFile $outFile
 Start-Process -FilePath C:\temp\DuoAuthProxy.exe /S
 Remove-Item "C:\Program Files\Duo Security Authentication Proxy\conf\authproxy.cfg"
-Invoke-Item "C:\Program Files\Duo Security Authentication Proxy\conf"
-Write-Host "ADD WINDOWS AD DIRECTORY SYNC..." -ForegroundColor Green
-Write-Host "Drop in the configuration file into the window that popped up." -ForegroundColor Green
-Write-Host "Continue once ready to start the Application Proxy." -ForegroundColor Green
+Invoke-Item "C:\Program Files\Duo Security Authentication Proxy\"
+
+Write-Host "Drop in the configuration file into the window that popped up, and make sure it's named 'authproxy.cfg' under the CONF folder." -ForegroundColor Yellow
 pause
 Start-Process -FilePath "C:\Program Files\Duo Security Authentication Proxy\bin\local_proxy_manager-win32-x64\Duo_Authentication_Proxy_Manager.exe"
 Write-Host "Enter the following IP address into the Duo Admin Portal"
 ipconfig
+Pause
+
+#Reveal DN
+Write-Host "When you're ready to grab the Distinguished Name, press enter." -BackgroundColor Yellow -ForegroundColor Black
+pause
 $filter = "(&(objectCategory=computer)(objectClass=computer)(cn=$env:COMPUTERNAME))"
 $dn = ([adsisearcher]$filter).FindOne().Properties.distinguishedname
 Write-Host "DN:   $($dn)" -ForegroundColor Cyan
+
+#Wait for user confirmation to continue
+Write-Host "When you're ready to edit the MSI file, press enter." -BackgroundColor Yellow -ForegroundColor Black
+pause
+
+#Download Orca
+$url = "https://www.technipages.com/downloads/OrcaMSI.zip"
+$outFile = "C:\temp\Orca.zip"
+Invoke-WebRequest -Uri $url -OutFile $outFile
+Expand-Archive $outFile -DestinationPath "C:\temp\DUO"
+
+#Run downloaded MSI for Orca
+$arguments = "/i `"C:\temp\DUO\Orca.msi`" /qb"
+Start-Process msiexec.exe -ArgumentList $arguments -Wait
+
+$ps = new-object System.Diagnostics.Process  
+$ps.StartInfo.Filename = "C:\Program Files (x86)\Orca\Orca.exe"  
+$ps.StartInfo.Arguments = "C:\DUO\DuoWindowsLogon64.msi"  
+$ps.start()
